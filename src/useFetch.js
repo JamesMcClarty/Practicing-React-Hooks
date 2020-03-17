0c9bf4476088816576260027dfbb639e5939d066
James McC <mariobud@gmail.com>
2020-03-17 17:14:24 -0500

    "Finished"

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,10 +3,14 @@ import {useState, useEffect} from "react"
 export default function useFetch(url){
     const [data, setData] = useState([]);
 
+    async function getData(){
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+    }
+
     useEffect(() =>{
-        fetch(url)
-        .then(response => response.json())
-        .then(data => setData(data))
+        getData();
     }, []);
 
     return data;
